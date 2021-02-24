fe57236779cd5dc1187053757c4c56366615d1aa
Allan Gaby <allan.gaby@gmail.com>
2021-02-24 10:16:24 -0300

    feat: criado estilo global

--- src/main/routes/router.tsx
+++ src/main/routes/router.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { OpenningRouter } from '@/presentation/routes'
+import GlobalStyle from '@/presentation/styles/global.styles'
 
 const Router: React.FC = () => {
   return (
     <BrowserRouter>
+      <GlobalStyle />
       <OpenningRouter />
     </BrowserRouter>
   )
