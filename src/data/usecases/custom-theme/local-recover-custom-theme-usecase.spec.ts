703921bbc5bfe2894cd449c94f6904bbce3db18f
Allan Gaby <allan.gaby@gmail.com>
2021-02-24 14:45:54 -0300

    test: garante que a LocalRecoverCustomThemeUseCase retorna o defaultTheme se nao encontrar um thema

--- src/data/usecases/custom-theme/local-recover-custom-theme-usecase.spec.ts
+++ src/data/usecases/custom-theme/local-recover-custom-theme-usecase.spec.ts
@@ -1,18 +1,23 @@
 import { LocalRecoverCustomThemeUseCase } from './local-recover-custom-theme-usecase'
 import { RecoverCustomThemeRepositoryStub } from '@/data/tests/custom-theme'
 import { throwError } from '@/data/tests/test-helper'
+import { ThemeModel } from '@/domain/models/custom-theme'
+import faker from 'faker'
 
 type sutTypes = {
   sut: LocalRecoverCustomThemeUseCase
   recoverCustomThemeRepositoryStub: RecoverCustomThemeRepositoryStub
+  defaultTheme: ThemeModel
 }
 
 const makeSut = (): sutTypes => {
   const recoverCustomThemeRepositoryStub = new RecoverCustomThemeRepositoryStub()
-  const sut = new LocalRecoverCustomThemeUseCase(recoverCustomThemeRepositoryStub)
+  const defaultTheme = faker.random.arrayElement([ThemeModel.dark, ThemeModel.light])
+  const sut = new LocalRecoverCustomThemeUseCase(recoverCustomThemeRepositoryStub, defaultTheme)
   return {
     sut,
-    recoverCustomThemeRepositoryStub
+    recoverCustomThemeRepositoryStub,
+    defaultTheme
   }
 }
 
@@ -31,11 +36,11 @@ describe('LocalRecoverCustomThemeUseCase', () => {
     await expect(promise).rejects.toThrow()
   })
 
-  test('Deve retornar undefined se o repositório não encontrar um thema', async () => {
-    const { sut, recoverCustomThemeRepositoryStub } = makeSut()
+  test('Deve retornar o defaultTheme se o repositório não encontrar um thema', async () => {
+    const { sut, recoverCustomThemeRepositoryStub, defaultTheme } = makeSut()
     recoverCustomThemeRepositoryStub.result = undefined
     const thema = await sut.getTheme()
-    expect(thema).toEqual(undefined)
+    expect(thema).toEqual(defaultTheme)
   })
 
   test('Deve retornar um thema se o repositório encontrar um thema', async () => {
