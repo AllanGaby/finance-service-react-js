1b6bdb8e795533b51e4e3cf498642d8f16afc63c
Allan Gaby <allan.gaby@uds.com.br>
2022-06-30 21:23:23 -0300

    chore: preparacao publicacão heroku

--- webpack.production.js
+++ webpack.production.js
@@ -1,5 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const common = require('./webpack.common')
+const Dotenv = require('dotenv-webpack')
+const { DefinePlugin } = require('webpack')
 const { merge } = require('webpack-merge')
 
 module.exports = merge(common(), {
@@ -9,6 +11,13 @@ module.exports = merge(common(), {
     'react-dom': 'ReactDOM'
   },
   plugins: [
+    new Dotenv(),
+    new DefinePlugin({
+      'process.env.BASE_URL': JSON.stringify(process.env.BASE_URL),
+      'process.env.ACCESS_TOKEN_KEY': JSON.stringify(process.env.ACCESS_TOKEN_KEY),
+      'process.env.ACCESS_TOKEN_NAME': JSON.stringify(process.env.ACCESS_TOKEN_NAME),
+      'process.env.SECURITY_PUBLIC_KEY': JSON.stringify(publicKey)
+    }),
     new HtmlWebpackPlugin({
       template: './src/presentation/common/templates/production.html',
       favicon: './src/assets/images/common/favicon.png'
