b7a714c8843dae827f21d905a799c5181bcf8299
Allan Gaby <allan.gaby@uds.com.br>
2022-06-30 21:26:31 -0300

    chore: preparacao publicacão heroku

--- webpack.production.js
+++ webpack.production.js
@@ -4,23 +4,27 @@ const Dotenv = require('dotenv-webpack')
 const { DefinePlugin } = require('webpack')
 const { merge } = require('webpack-merge')
 
-module.exports = merge(common(), {
-  mode: 'production',
-  externals: {
-    react: 'React',
-    'react-dom': 'ReactDOM'
-  },
-  plugins: [
-    new Dotenv(),
-    new DefinePlugin({
-      'process.env.BASE_URL': JSON.stringify(process.env.BASE_URL),
-      'process.env.ACCESS_TOKEN_KEY': JSON.stringify(process.env.ACCESS_TOKEN_KEY),
-      'process.env.ACCESS_TOKEN_NAME': JSON.stringify(process.env.ACCESS_TOKEN_NAME),
-      'process.env.SECURITY_PUBLIC_KEY': JSON.stringify(publicKey)
-    }),
-    new HtmlWebpackPlugin({
-      template: './src/presentation/common/templates/production.html',
-      favicon: './src/assets/images/common/favicon.png'
-    })
-  ]
-})
+module.exports = async () => {
+  const publicKey = await fs.promises.readFile('./public_key.pem', { encoding: 'utf-8' })
+  return {
+    ...common(),
+    mode: 'production',
+    externals: {
+      react: 'React',
+      'react-dom': 'ReactDOM'
+    },
+    plugins: [
+      new Dotenv(),
+      new DefinePlugin({
+        'process.env.BASE_URL': JSON.stringify(process.env.BASE_URL),
+        'process.env.ACCESS_TOKEN_KEY': JSON.stringify(process.env.ACCESS_TOKEN_KEY),
+        'process.env.ACCESS_TOKEN_NAME': JSON.stringify(process.env.ACCESS_TOKEN_NAME),
+        'process.env.SECURITY_PUBLIC_KEY': JSON.stringify(publicKey)
+      }),
+      new HtmlWebpackPlugin({
+        template: './src/presentation/common/templates/production.html',
+        favicon: './src/assets/images/common/favicon.png'
+      })
+    ]
+  }
+}
