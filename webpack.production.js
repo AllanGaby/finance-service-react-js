c45b93e069094a312045e26afbbe252bd68801b8
Allan Gaby <allan.gaby@uds.com.br>
2022-07-26 09:55:20 -0300

    fix: corrigida validacão de versão do app

--- webpack.production.js
+++ webpack.production.js
@@ -1,19 +1,50 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const common = require('./webpack.common')
 const fs = require('fs')
+const path = require('path')
 const Dotenv = require('dotenv-webpack')
 const { DefinePlugin } = require('webpack')
+const CopyPlugin = require('copy-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = async () => {
   const publicKey = await fs.promises.readFile('./public_key.pem', { encoding: 'utf-8' })
   return {
-    ...common(),
+    entry: './src/main/index.tsx',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'main-bundle-[fullhash].js',
+      publicPath: ''
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js'],
+      alias: {
+        '@': path.join(__dirname, 'src')
+      }
+    },
+    module: {
+      rules: [{
+        test: /\.ts(x?)$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      }]
+    },
     mode: 'production',
     externals: {
       react: 'React',
       'react-dom': 'ReactDOM'
     },
+    devServer: {
+      contentBase: './public',
+      writeToDisk: true,
+      historyApiFallback: true
+    },
     plugins: [
+      new CleanWebpackPlugin(),
+      new CopyPlugin({
+        patterns: [
+          { from: 'src/assets', to: 'assets' }
+        ]
+      }),
       new Dotenv(),
       new DefinePlugin({
         'process.env.BASE_URL': JSON.stringify(process.env.BASE_URL),
@@ -22,7 +53,7 @@ module.exports = async () => {
         'process.env.SECURITY_PUBLIC_KEY': JSON.stringify(publicKey)
       }),
       new HtmlWebpackPlugin({
-        template: './src/presentation/common/templates/production.html',
+        template: './src/main/application/templates/production.html',
         favicon: './src/assets/images/common/favicon.png'
       })
     ]
